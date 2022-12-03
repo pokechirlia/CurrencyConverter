4d177560cff8962873e9cdb07fa071c232a361da
pokechirlia <chi.k.tran2000@gmail.com>
2022-12-03 15:12:15 +1100

    setup api key and retrieve data

--- src/currencyconverter/Converter.java
+++ src/currencyconverter/Converter.java
@@ -5,8 +5,13 @@
  */
 package currencyconverter;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.Scanner;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -18,8 +23,27 @@ public class Converter {
     public Converter(){
         try {
             URL url = new URL("https://api.apilayer.com/currency_data/convert?to=VND&from=AUD&amount=5");
-            System.out.println("its running here");
+            HttpURLConnection con = (HttpURLConnection)url.openConnection();
+            con.setRequestProperty("apikey", "boDU9Vlt8MUpJLStiXGWzZgRCqTgLUiA");
+            con.setRequestMethod("GET");
+            
+            con.connect();
+            
+            int responseCode = con.getResponseCode();
+            System.out.println(responseCode);
+            
+            if(responseCode == 200)
+            {
+                BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
+                //StringBuffer response = new StringBuffer();
+                String inputLine;
+                while((inputLine = in.readLine()) != null){
+                System.out.println(inputLine);
+            }
+            }
         } catch (MalformedURLException ex) {
+            Logger.getLogger(Converter.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (IOException ex) {
             Logger.getLogger(Converter.class.getName()).log(Level.SEVERE, null, ex);
         }
     }
